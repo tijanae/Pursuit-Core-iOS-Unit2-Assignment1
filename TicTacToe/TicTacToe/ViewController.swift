ec24f65f5de95e178ba686621f1d219f4754e275
Tia Lendor <TiaLendon@pursuit.org>
2019-08-06 16:16:16 -0400

    1st commit

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,7 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
+    var activePlayer = 1 // cross
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    //gameState represents the places on the board, not
+  
+    @IBAction func buttonXes(_ sender: UIButton) {
+        
+        if gameState[sender.tag-1] == 0{
+            if activePlayer == 1{
+                sender.setImage(UIImage(named: "Player X"), for: UIControl.State())
+                activePlayer = 2
+            }
+            else{
+                sender.setImage(UIImage(named: "Player O"), for: UIControl.State())
+                activePlayer = 1
+            }
+        }
+        
+    }
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
