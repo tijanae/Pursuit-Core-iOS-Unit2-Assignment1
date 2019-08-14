fd13f893c405c8ac5551a890cc4654bc3bd72e2f
Tia Lendor <TiaLendon@pursuit.org>
2019-08-13 20:13:32 -0400

    almost done

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -7,7 +7,39 @@
 //
 
 import Foundation
+import UIKit
+
+enum Space {
+   
+    case playerX
+    case playerO
+    case empty
+    
+}
+
 
 class TicTacToeBrain {
   
+    var gameState = [Space.empty, Space.empty, Space.empty, Space.empty, Space.empty, Space.empty, Space.empty, Space.empty, Space.empty]
+    
+    
+    let winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                        [0, 4, 8], [2, 4, 6]]
+    
+    
+        func checkingWinngCombo () -> Bool {
+            for combination in winningCombos{
+//                print(gameState)
+//                print(combination[0])
+//
+//                print(combination[1])
+                if gameState[combination[0]] != Space.empty && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
+                    return true
+                }
+            }
+            return false
+    }
+    
+   
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,23 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var activePlayer = 1 // cross
-    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    //gameState represents the places on the board, not
-  
+    var brainy = TicTacToeBrain()
+    var activePlayer = Space.playerX
+    
+    @IBOutlet weak var playerMessage: UILabel!
+    
+    //gameState represents the places on the board
+
+  //
     @IBAction func buttonXes(_ sender: UIButton) {
         
-        if gameState[sender.tag-1] == 0{
-            if activePlayer == 1{
+        if brainy.gameState[sender.tag - 1] == .empty{
+        brainy.gameState[sender.tag-1] = activePlayer
+            
+            if activePlayer == Space.playerX{
                 sender.setImage(UIImage(named: "Player X"), for: UIControl.State())
-                activePlayer = 2
+                activePlayer = Space.playerO
             }
             else{
                 sender.setImage(UIImage(named: "Player O"), for: UIControl.State())
-                activePlayer = 1
+                activePlayer = Space.playerX
             }
         }
+        if brainy.checkingWinngCombo()  {
+            playerMessage.text = "PLAYER WINS!"
+            sender.isEnabled = false
+        }
         
+        
+//        for combination in winningCombos{
+//            brainy.checkingWinngCombo()
+//            print(gameState)
+//            print(combination[0])
+//            
+//            print(combination[1])
+//            if gameState[combination[0]] != Space.empty && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
+//                
+//                
+//            }
+//        }
+    
     }
     
     override func viewDidLoad() {
